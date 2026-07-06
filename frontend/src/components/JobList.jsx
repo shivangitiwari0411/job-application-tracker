213a196ab6ebdbb006337bfafddcccb1347185f3
Shivangi Tiwari <shivangi.tiwari0411@gmail.com>
2026-07-06 15:16:19 +0530

    Use axios interceptor for authenticated API requests

--- frontend/src/components/JobList.jsx
+++ frontend/src/components/JobList.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import API from "../services/api";
+import API from "../api/axios";
 import { toast } from "react-toastify";
 import StatusChart from "../charts/StatusChart";
 import * as XLSX from "xlsx";
